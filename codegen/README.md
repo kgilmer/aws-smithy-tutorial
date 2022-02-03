820de5c5cde9165b33e4f7da1296c4da9fdcf54a
Ken Gilmer <kggilmer@amazon.com>
2022-02-02 18:08:16 -0800

    Generate graphviz dot diagram

--- codegen/README.md
+++ codegen/README.md
@@ -1,1 +1,36 @@
 This Gradle module represents the codegen plugin for Smithy.  It integrates with Smithy to generate code.
+
+This codegen project will generate graphviz dot diagrams from API models.  An example diagram in dot format:
+
+```
+digraph D {
+
+  subgraph cluster_p {
+    label = "Parent";
+
+    subgraph cluster_c1 {
+      label = "Child one";
+      a;
+
+      subgraph cluster_gc_1 {
+        label = "Grand-Child one";
+         b;
+      }
+      subgraph cluster_gc_2 {
+        label = "Grand-Child two";
+          c;
+          d;
+      }
+
+    }
+
+    subgraph cluster_c2 {
+      label = "Child two";
+      e;
+    }
+  }
+} 
+```
+
+This type of diagram may be useful to view relationships across entities and containment.
+In our codegen project we'll model services -> operations - [inputs, outputs, errors] -> Shapes
--- codegen/src/main/kotlin/org/example/smithy/CodegenPlugin.kt
+++ codegen/src/main/kotlin/org/example/smithy/CodegenPlugin.kt
@@ -2,11 +2,86 @@ package org.example.smithy
 
 import software.amazon.smithy.build.PluginContext
 import software.amazon.smithy.build.SmithyBuildPlugin
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.MemberShape
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.model.shapes.ServiceShape
+import software.amazon.smithy.model.shapes.ShapeId
+import software.amazon.smithy.model.shapes.StructureShape
 
 class CodegenPlugin : SmithyBuildPlugin {
     override fun getName(): String = "example-codegen"
 
     override fun execute(context: PluginContext?) {
-        // shake your money maker
+        requireNotNull(context) { "Smithy PluginContext was unexpectedly null" }
+        require(context.model.serviceShapes.size == 1) {
+            "Can only generate a graph for a single service, but model contains ${context.model.serviceShapes.size} services."
+        }
+
+        generateDotDiagramForService(context.model)
+    }
+
+    private fun generateDotDiagramForService(model: Model) {
+        val service = model.serviceShapes.first()
+
+        println("""
+            digraph D {
+                subgraph cluster_service {
+                    label = "${service.id.name}";
+
+                    ${generateOperationContainer(model, service)}
+                }
+            }
+        """.trimIndent())
+    }
+
+    private fun generateOperationContainer(model: Model, service: ServiceShape): String {
+        return """
+            subgraph cluster_operations {
+            label = "Operations";
+
+            ${service
+                .allOperations
+                .map {  generateOperation(model, it) }.joinToString("\n") { it }
+            }
+            }
+        """.trimIndent()
+    }
+
+    private fun generateOperation(model: Model, operationShapeId: ShapeId): String {
+        val operationShape = model.expectShape(operationShapeId, OperationShape::class.java)
+        val inputShape = operationShape.input.orElseGet { null } ?: error("Expected input shape")
+        val outputShape = operationShape.output.orElseGet { null } ?: error("Expected output shape")
+        return """
+            subgraph cluster_${operationShapeId.name}Operation {
+                label = "${operationShapeId.name}";
+
+                subgraph cluster_o1 {
+                    label = "Inputs";
+
+                    subgraph cluster_${inputShape.name} {
+                        label = "${inputShape.name}";
+
+                        ${generateMembers(model, inputShape)}
+                    }
+                }
+
+                subgraph cluster_o2 {
+                    label = "Outputs";
+
+                    subgraph cluster_${outputShape.name} {
+                        label = "${outputShape.name}";
+
+                        ${generateMembers(model, inputShape)}
+                    }
+                }
+            }
+        """.trimIndent()
+    }
+
+    private fun generateMembers(model: Model, shapeId: ShapeId): String {
+        val shape = model.expectShape(shapeId, StructureShape::class.java)
+
+        return shape.members().joinToString(";\n") { it.id.name }
     }
 }
